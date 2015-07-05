c00e588b5483b34a6e75b7a12bff09f0efe1062c
Robciuc Ioan <ionut@9pixels.co>
2015-07-05 09:12:55 +0300

    Issue (font serif pe headline-uri nu ai folosit. Vad ca tema are unul aici:) fixed.

--- functions.php
+++ functions.php
@@ -26,17 +26,7 @@ if( !function_exists( 'minimalzerif_theme_setup' ) ) {
 if( !function_exists( 'minimalzerif_enqueue_styles' ) ) {
 	add_action( 'wp_enqueue_scripts', 'minimalzerif_enqueue_styles' );
 	function minimalzerif_enqueue_styles() {
-		// Google Fonts
-		$google_fonts_args = array(
-			'family'	=> 'Roboto:700'
-		);
-
-		// WP Register Style
-		wp_register_style( 'google-fonts', add_query_arg( $google_fonts_args, "//fonts.googleapis.com/css" ), array(), null );
-
-		// WP Enqueue Style
         wp_enqueue_style( 'minimalzerif-style', GET_CHILDTHEME_DIRECTORY_URI . '/style.css', array( 'zerif_style' ), '1.0.0', 'all' );
         wp_enqueue_style( 'zerif_style', get_template_directory_uri() . '/style.css' );
-        wp_enqueue_style( 'google-fonts' );
 	}
 }
