9e2df071b098792fb651d32a93f0ca6b5f8413d3
Robciuc Ioan <ionut@9pixels.co>
2015-07-06 09:40:54 +0300

    Issue (Fundalul la header sa fie transparent, ramane alb la scroll) fixed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 /**
  *	jQuery Ready
  */
-jQuery( document ).ready( function($) {
+jQuery(document).ready(function() {
+
+	jQuery( window ).scroll( function() {
+		if( ( jQuery( window ).scrollTop() ) > 0 ) {
+			jQuery( '#main-nav' ).addClass( 'fix' );
+		}
+	});
+
 });
