a8177c3a94da066001ce04b5385ad7c514ff8617
Robciuc Ioan <ionut@9pixels.co>
2015-07-09 06:12:51 +0300

    Fixed: - I generated languages files; - I modify README.md; - I created README.txt.

---
+++ footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *	The template for displaying the Footer.
+ *
+ *	@package minimalzerif
+ */
+?>		
+		<footer id="footer">
+			<div class="container">
+				<div class="zerif-copyright-box">
+					MinimalZerif <?php _e( 'powered by', 'minimalzerif' ); ?> <a href="http://www.wordpress.org" title="WordPress" target="_blank" rel="nofollow">WordPress</a>.
+				</div><!--/.zerif-copyright-box-->
+			</div> <!-- / END CONTAINER -->
+		</footer> <!-- / END FOOOTER  -->
+	<?php wp_footer(); ?>
+	</body>
+</html>
